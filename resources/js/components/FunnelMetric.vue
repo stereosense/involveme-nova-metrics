b5c137c519443ee7c2e06e78feb37206e9d95deb
Theofanis <vardtheo@gmail.com>
2025-01-22 18:00:17 +0200

    changed formatting to 4 items per line

--- resources/js/components/FunnelMetric.vue
+++ resources/js/components/FunnelMetric.vue
@@ -14,19 +14,21 @@
         </div>
 
 
-        <div class="flex items-center justify-between px-6">
-            <div v-for="(metricData, idx) in metricsData" :key="idx" class="flex space-x-8">
-                <div class="flex items-center space-x-6">
+        <div class="flex flex-wrap items-center justify-start px-6">
+            <div v-for="(metricData, idx) in metricsData" :key="idx" class="flex items-center justify-between h-14 w-1/4 ">
+                <div class="flex justify-between flex-1 items-center space-x-3">
                     <span class="text-4xl">{{ formatNumber(metricData.value) }}</span>
-                    <div class="flex flex-col text-center">
-                        <a v-if="metricData.link" :href="metricData.link" :title="metricData.tooltip" target="_blank" class="link-default">{{ metricData.name }}</a>
-                        <span v-else class="text-base" :title="metricData.tooltip">{{ metricData.name }}</span>
-                        <span class="text-sm" title="Compared to previous">{{calculatePercentageToPrevious(idx)}}%</span>
-                        <span v-if="![0,1].includes(idx)" class="text-sm" title="Compared to first">/ {{calculatePercentageToFirst(idx)}}%</span>
+                    <div class="flex flex-col flex-grow text-center">
+                        <a v-if="metricData.link" :href="metricData.link" :title="metricData.tooltip" target="_blank" class="link-default flex-grow">{{ metricData.name }}</a>
+                        <span v-else :title="metricData.tooltip" class="flex-grow">{{ metricData.name }}</span>
+                      <div>  
+                        <span class="text-sm" title="Compared to previous">{{calculatePercentageTo(idx,idx-1)}}%</span>
+                        <span v-if="idx > 1" class="text-sm" title="Compared to first">&nbsp;/ {{calculatePercentageTo(idx,0)}}%</span>
+                      </div>
                     </div>
                 </div>
                 <div v-if="idx !== (metricsData.length - 1)">
-                    <svg class="w-14 h-14" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8.25 4.5l7.5 7.5-7.5 7.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                    <svg class="w-12 h-12" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8.25 4.5l7.5 7.5-7.5 7.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
                 </div>
             </div>
         </div>
@@ -72,26 +74,17 @@
                         console.log('error', error);
                     });
             },
-            calculatePercentageToPrevious(idx) {
+            calculatePercentageTo(idx, idy) {
                 if(idx === 0) {
                     return 100;
                 }
-                if(this.metricsData[idx - 1].value === 0) {
+                if(this.metricsData[idy].value === 0) {
                     return 0;
                 }
-                return Math.round((this.metricsData[idx].value / this.metricsData[idx - 1].value) * 10000) / 100;
-            },
-            calculatePercentageToFirst(idx) {
-                if(idx === 0) {
-                    return 100;
-                }
-                if(this.metricsData[0].value === 0) {
-                    return 0;
-                }
-                return Math.round((this.metricsData[idx].value / this.metricsData[0].value) * 10000) / 100;
+                return Math.round((this.metricsData[idx].value / this.metricsData[idy].value) * 10000) / 100;
             },
             formatNumber(number) {
-                return new Intl.NumberFormat('us-US', {maximumFractionDigits:0}).format(number);
+                return new Intl.NumberFormat('us-US', {maximumFractionDigits: 0}).format(number);
             }
         }
     };
